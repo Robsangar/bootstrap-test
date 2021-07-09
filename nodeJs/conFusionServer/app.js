c4659b93a31a54b2f36e8aaee36f2705acc9433c
robsangar <ravinder.sangar@hotmail.com>
2021-07-09 13:35:18 +0200

    passport jwt

--- nodeJs/conFusionServer/app.js
+++ nodeJs/conFusionServer/app.js
@@ -12,6 +12,7 @@ var promotionRouter = require('./routes/promotionRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var passport = require('passport');
 var authenticate = require('./authenticate');
+var config = require("./config");
 
 
 const mongoose = require('mongoose');
@@ -19,7 +20,7 @@ const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 const { nextTick } = require('process');
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
@@ -35,42 +36,18 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
 
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function auth(req, res, next) {
-
-  if (!req.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 401;
-    next(err);
-    return;
-  }
-  else {
-    next();
-  }
-}
-
-
-app.use(auth);
-
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.use('/dishes', dishRouter);
 app.use('/promotions', promotionRouter);
 app.use('/leaders', leaderRouter);
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
